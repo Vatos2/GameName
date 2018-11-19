4a87da6b5654b674d0de40cac89207060cea56a2
Hyacinth <scorchedsilver@gmail.com>
2018-11-19 03:01:49 -0800

    Movement system, some fixes, action system skeleton added.

--- Stranded_Objects.py
+++ Stranded_Objects.py
@@ -27,6 +27,10 @@ class Player:
 
     def pickup(self,item):
         self.inventory = self.inventory.append(item)
+        print("You got the ", "<",item.name,">", "!")
+
+    def listInventory(self):
+        print(self.inventory)
 
 
 
@@ -39,10 +43,33 @@ class Loc:
                 self.interactions = interactions # Probably an array of "general" interactions
                 self.state = state # 0 = Passable, 1 = Permanently Impassable, 2 = Temporarily Impassable
                 self.coords = [-1, -1] # Location on the map.
-                self.explored = 0 # Has the player "Discovered" this.
+                self.explored = 0 # Has the player "Discovered" this 0 for no 1 for yes.
 
     def getCoords(self):
         return self.coords
+
+    def isPassable(self):
+        if self.state == 0:
+            return True
+
+        elif self.state == 1:
+            print("That location is impassable!")
+            return False
+        elif self.state == 2:
+            print("That location is impassable... for now.")
+            return False
+    def isExplored(self):
+        if self.explored == 0:
+            return False
+        else:
+            return True
+
+    def discover(self):
+        self.explored = 1
+        print("You've discovered the ", self.name)
+
+
+
 class Item:
     #Items
 
@@ -50,3 +77,57 @@ class Item:
         self.name = name
         self.desc = description
 
+class Instance:
+    # The "Game" Object, will allow us to pass our initialization into the main game function.
+    # All we need to pass to the instance are the player, and the map since those two objects contain all of our objs
+    # If you're unsure where to put any functions, it's probably safe to assume you can put it here.
+
+    def __init__(self, player, map):
+        self.player = player
+        self.map = map
+        self.playerx = self.player.location.coords[0]
+        self.playery = self.player.location.coords[1]
+
+    def updatePlayerLoc(self,x,y):
+        self.player.setLocation(self.map[x][y])
+        self.playerx = self.player.location.getCoords[0]
+        self.playery = self.player.location.getCoords[1]
+
+    def move(self,direction):
+        # For moving the player around, we'll take as input cardinal directions N,W,E,S.
+        # Remember len(map) is y length, len(map[0]) is xlength!
+        direction = direction.upper()
+        if direction == "N":
+            if (self.player.playery + 1 < len(self.map)):
+                if (self.map[self.player.playerx][self.player.playery + 1]).isPassable():
+                    self.updatePlayerLoc(self.player.playerx,self.player.playery + 1)
+                    print("You move north.")
+                    if self.player.location.isExplored():
+                        self.player.location.discover()
+
+
+        elif direction == "S":
+            if (self.player.playery - 1 > 0):
+                if (self.map[self.player.playerx][self.player.playery - 1]).isPassable():
+                    self.updatePlayerLoc(self.player.playerx,self.player.playery - 1)
+                    print("You move south.")
+                    if self.player.location.isExplored():
+                        self.player.location.discover()
+                    print("You are now in: ", self.player.location.name)
+        elif direction == "W":
+            if (self.player.playerx - 1 > 0):
+                if (self.map[self.player.playerx - 1][self.player.playery]).isPassable():
+                    self.updatePlayerLoc(self.player.playerx-1,self.player.playery)
+                    print("You move west.")
+                    if self.player.location.isExplored():
+                        self.player.location.discover()
+                    print("You are now in: ", self.player.location.name)
+
+        elif direction == "E":
+            if (self.player.playerx + 1 < len(self.map[0])):
+                if (self.map[self.player.playerx + 1][self.player.playery]).isPassable():
+                    self.updatePlayerLoc(self.player.playerx + 1, self.player.playery)
+                    print("You move east.")
+                    if self.player.location.isExplored():
+                        self.player.location.discover()
+                    print("You are now in: ", self.player.location.name)
--- main.py
+++ main.py
@@ -7,8 +7,11 @@ Brief:
 In Stranded, you find yourself marooned on an island through some unfortunate events. In this game, your goal is to
 survive and escape the island.
 
-DEV NOTE:
+DEV NOTES:
 I encourage you to look at the game concepts document, and reach out to us on discord if you have any questions.
+
+STATUS UPDATES:
+Finished framework for movement system, working on implementing a loop for in game actions.
 
 """
 
@@ -16,20 +19,68 @@ import Stranded_Objects as SO
 import random
 
 def initMap(myMap):
-   for i in range(len(myMap)):
-       for j in range(len(myMap[i])):
-           (myMap[i][j]).coords = [i, j]
+       for i in range(len(myMap)):
+           for j in range(len(myMap[i])):
+               (myMap[i][j]).coords = [i, j]
+       return myMap
 def emptyMap(x,y):
     myMap = []
     myMap = [["NULL" for i in range(x)] for j in range(y)]
     return myMap
+
+def startGame(game):
+    player = game.player # for easier accessing
+    map = game.map # for easier accessing
+
+    print("Five days ago, your cruise ship was caught in a terrible storm and torn apart.\n"
+          "you managed to make it to a life boat alone, and you have no clue of the fate of your fellow passengers.\n"
+          "Before you escaped you managed to take one item with you, what was it?\n")
+
+    print("1. A repair kit for electronics. \n"
+          "2. A machete \n"
+          "3. A first aid kit. \n")
+
+    machete = SO.Item("Machete", "A bladed tool used for clearing brush.")
+    eKit = SO.Item("Electronics Kit", "A tool kit, which could be used to repair electronic equipment.")
+    firstAid = SO.Item("First-Aid Kit", "Contains various medical supplies which can be used to heal you.")
+    shovel = SO.Item("Shovel","A shovel, generally used for digging.")
+    firstItem = input()
+    if firstItem == "1":
+        player.pickup(eKit)
+
+    elif firstItem == "2":
+        player.pickup(machete)
+
+    elif firstItem == "3":
+        player.pickup(firstAid)
+    else:
+        print("Invalid entry, for your failure you will receive a random item.")
+
+    while True:
+        print("What would you like to do?")
+        print("1. Check Surroundings.\n"
+              "2. Move to a different location.\n"
+              "3. View your inventory.\n"
+              "4. Use an item")
+        action = input()
+        if action == "1":
+            continue
+
+        elif action == "2":
+            continue
+
+        elif action == "3":
+            player.listInventory()
+
+        elif action == "4":
+            continue
 
 def initialize():
     print("Initializing...")
 
     # >>> LOCATION BUILDING STARTS HERE <<<
     #  These are location templates, we can use these when we're creating new locations, but we shouldn't use plain
-    #  Templates as a location, examples of this shown below.
+    #  templates as a location, examples of this shown below.
     #  Permanently Impassable locations should be prefaced with i_
     #  Temporarily impassable locations should be prefaced with t_
     #  Open locations should be prefaced with n_
@@ -40,8 +91,8 @@ def initialize():
     n_rad = SO.Loc("Radio Tower", "An abandoned radio tower",[], 0)
     t_occ = SO.Loc("Ocean", "The ocean, stretches on as far as the eye can see",[],2)
     # >>> MAP BUILDING AND INFORMATION <<<
-    # TESTING MAP!!
 
+    # Map for testing and dev work.
     # The demo map, the only purpose of the definition here is for outlining what will be in it once we're done
     # Dimensions: 3x4
     DemoMap = \
@@ -66,14 +117,18 @@ def initialize():
                         [lav1, bch1, lav6],
                         [occ1, occ2, occ3]
         ]
-
     DemoMap = initMap(DemoMap)
 
     IslandMap = [[]]  # Our map will be a 2D array of locations.
 
+    IslandMap = DemoMap # For development I use the demomap for testing, will be changed once we have real maps
+    print("Map initialized at ylen: ", len(IslandMap), " xlen: ", len(IslandMap[0]))
     player = SO.Player([],0,100,20,n_bch)  # Make the player character.
 
+    return player, IslandMap  # What we are going to pass to the game function.
+
     print("Initializing finished...")
+
 def main(): # Main will be our Main Menu
     print("Welcome to \'Stranded\'!")
     print("Make a selection:\n"
@@ -81,7 +136,12 @@ def main(): # Main will be our Main Menu
           "\t2. Exit\n")
     selection = input()
     if selection == "1": # Input only takes strings.
-        initialize()
+        player,map = initialize() # Initialize returns two things, player and map.
+        game = SO.Instance(player, map) # Create the game instance
+        startGame(game)
+        # By splitting up our main and our game, we can send the player back to the menu using main()
+
+
     else:
         exit("Thanks for playing!")
 
